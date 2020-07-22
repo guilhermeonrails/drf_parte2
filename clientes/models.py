8dab79fab156665a5415ce8311a082f7a4dcb851
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-07-22 16:06:59 -0300

    final da aula 1

--- clientes/models.py
+++ clientes/models.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=9)
-    rg = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
+    rg = models.CharField(max_length=9, unique=True)
     celular = models.CharField(max_length=11)
     ativo = models.BooleanField(default=False)
     data_cadastro = models.DateTimeField(default=datetime.now, editable=False)
--- clientes/serializers.py
+++ clientes/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from clientes.models import Cliente
 
 class ClienteSerializer(serializers.ModelSerializer):
-    """Exibindo todos os clientes"""
     class Meta:
         model = Cliente
         fields = '__all__'
+    def validate_celular(self, celular):
+        if len(celular) != 9:
+            raise serializers.ValidationError("O número de celular deve ter 9 dígitos")
+        return celular
+    def validate_nome(self, nome):
+        if nome.replace(" ", "").isalpha():
+            raise serializers.ValidationError("Nome inválido")
+        return nome
